1e588d429869d844d3270571f75747bf031fe8a4
Ryndebstin <61210530+Ryndebstin@users.noreply.github.com>
2020-05-30 18:28:37 +0200

    Delete hashkrack.py

--- hashkrack.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-f = open('Wordlist', 'r+')
-liste = f.readlines()
-hash=input('type hash : ')
-for i in range(0,len(liste) + 1):
-	if hashlib.sha224(liste[i][0:-1].encode('utf-8')).hexdigest() == hash:
-		print("password : " + liste[i])
-		exit()
-print("no password")
