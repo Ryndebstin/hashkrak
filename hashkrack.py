ac0d2c8a55ebc650bad844137c6df92b8ae4bf2b
Ryndebstin <61210530+Ryndebstin@users.noreply.github.com>
2020-05-30 18:20:56 +0200

    Create hashkrack.py

---
+++ hashkrack.py
@@ -0,0 +1,9 @@
+import hashlib
+f = open('Wordlist.txt', 'r+')
+liste = f.readlines()
+hash=input('type hash : ')
+for i in range(0,len(liste) + 1):
+	if hashlib.sha224(liste[i][0:-1].encode('utf-8')).hexdigest() == hash:
+		print("password : " + liste[i])
+		exit()
+print("no password")
